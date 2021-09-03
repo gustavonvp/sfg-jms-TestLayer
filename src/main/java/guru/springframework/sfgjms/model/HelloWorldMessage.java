c4cb32baceeae11460512fccf13eed97e7dbd11e
gustavonvp <gustavonvp@gmail.com>
2021-09-03 13:20:57 -0300

    Adding Serializer to use Json  Closes #3

--- src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -5,14 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class HelloWorldMessage {
+public class HelloWorldMessage implements Serializable {
 
+    static final long serialVersionUID = 871230127831L;
     private UUID id;
     private String message;
 }
