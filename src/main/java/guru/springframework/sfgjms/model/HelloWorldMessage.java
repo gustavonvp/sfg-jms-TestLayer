721ce37f4744cf9f07d4693d0a8185b291465629
gustavonvp <gustavonvp@gmail.com>
2021-09-03 13:19:04 -0300

    Adding POJO  Closes #2

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage {
+
+    private UUID id;
+    private String message;
+}
